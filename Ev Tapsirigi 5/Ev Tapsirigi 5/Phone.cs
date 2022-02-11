13faa2f8230f282221f8be5f265c566e4920938c
Mammadli999 <abbasfm@code.edu.az>
2022-02-12 03:49:34 +0400

    Ev Tapsirigi 5

---
+++ Ev Tapsirigi 5/Ev Tapsirigi 5/Phone.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ev_Tapsirigi_5
+{
+    internal class Phone
+    {
+        public int Id;
+        public string Model;
+        public string Producer;
+        public int MainCamera;
+        public int Ram;
+        public int Memory;
+        public int ProductionYear;
+        public string OperatingSystem;
+        public int BatteryCapacity;
+        public double Price;
+        public static int counter = 0;
+
+        public Phone(string Model,string Producer,int MainCamera,int Ram, int Memory, int ProductionYear, string OperatingSystem, int BatteryCapacity, double Price)
+        {
+            counter++;
+            Id = counter;
+            this.Model = Model;
+            this.Producer = Producer;
+            this.MainCamera = MainCamera;
+            this.Ram = Ram;
+            this.Memory = Memory;
+            this.ProductionYear = ProductionYear;
+            this.OperatingSystem = OperatingSystem;
+            this.BatteryCapacity = BatteryCapacity;
+            this.Price = Price;
+        }
+
+        public override string ToString()
+        {
+            return $"Telefon: {Id}\n" +
+                $"Model: {Model}\n" +
+                $"Istehsalci: {Producer}\n" +
+                $"Esas Kamera: {MainCamera}\n" +
+                $"Ram: {Ram}\n" +
+                $"Yaddas: {Memory}\n" +
+                $"Buraxilis ili: {ProductionYear}\n" +
+                $"Emeliyyat Sistemi: {OperatingSystem}\n" +
+                $"Batareya Tutumu: {BatteryCapacity}\n" +
+                $"Qiymet:{Price}";
+        }
+    }
+}
---
+++ Ev Tapsirigi 5/Ev Tapsirigi 5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ev_Tapsirigi_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Phone phone1 = new Phone("Iphone 13 Pro","Amerika",12,6,256,2021,"IOS",3125,3219.99);
+            Phone phone2 = new Phone("Iphone 11 Pro", "Amerika", 12, 4, 64, 2019, "IOS", 3046, 2549.99);
+            Phone phone3 = new Phone("Galaxy Z Fold 3", "Korea", 12, 12, 256, 2021, "Android", 4400, 4199.99);
+
+            Phone[] phones = new Phone[3];
+
+            phones[0] = phone1;
+            phones[1] = phone2;
+            phones[2] = phone3;
+
+            foreach (var item in phones)
+            {
+                Console.WriteLine(item);
+                Console.WriteLine("---------------------------------");
+            }
+        }
+    }
+}
